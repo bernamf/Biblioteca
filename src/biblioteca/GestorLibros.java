c09d995707ee946ec814352d4eecaeb96c37921d
benat merino <ikctr@plaiaundi.net>
2024-02-08 10:19:10 +0100

    MENU DE LIBRO TERMINADO

--- src/biblioteca/GestorLibros.java
+++ src/biblioteca/GestorLibros.java
@@ -1,9 +1,36 @@
 package biblioteca;
 
+import java.util.Scanner;
+
 public class GestorLibros {
 
 	public static void run() {
 		
-	}
-	
+		 Scanner scanner = new Scanner(System.in);
+	        int opcion;
+	        
+	        do {
+	        	Menu.mostrarMenuLibros();
+	            System.out.print("Ingrese su opción: ");
+	            opcion = scanner.nextInt();
+	            switch (opcion) {
+	                case Menu.INSERTAR_LIBRO:
+	                   
+	                    break;
+	                case Menu.ELIMINAR_LIBRO:
+		                   
+	                    break;
+	                case Menu.VER_LIBRO:
+		                   
+	                    break;
+	              
+	                default:
+	                    System.out.println("Opción no válida. Por favor, ingrese una opción válida.");
+	            }
+	        } while (opcion != 0);
+	        scanner.close();
+	    }
+		
 }
+	
+
--- src/biblioteca/Menu.java
+++ src/biblioteca/Menu.java
@@ -3,23 +3,28 @@ package biblioteca;
 public class Menu {
 	
 	public static final int SALIR = 0;
+	
 	public static final int GESTION_LIBROS = 1;
 	public static final int GESTION_SOCIOS = 2;
 	public static final int GESTION_PRESTAMOS =3;
 	
+	public static final int INSERTAR_LIBRO=1;
+	public static final int ELIMINAR_LIBRO=2;
+	public static final int VER_LIBRO=3;
+	
     public static void mostrarMenuPrincipal() {
         System.out.println("Menu Principal");
         System.out.println(GESTION_LIBROS + ". Gestionar Libros");
-        System.out.println("2. Gestionar Socios");
-        System.out.println("3. Gestionar Prestamos");
+        System.out.println(GESTION_SOCIOS +". Gestionar Socios");
+        System.out.println(GESTION_PRESTAMOS +". Gestionar Prestamos");
     }
 
     public static void mostrarMenuLibros() {
         System.out.println("Menu Libros");
-        System.out.println("1. Insertar libro");
-        System.out.println("2. Eliminar libro");
-        System.out.println("3. Ver libros");
-        System.out.println("4. Volver al menú principal");
+        System.out.println(INSERTAR_LIBRO +". Insertar libro");
+        System.out.println(ELIMINAR_LIBRO +". Eliminar libro");
+        System.out.println(VER_LIBRO +". Ver libros");
+        System.out.println(SALIR + ". Volver al menú principal");
     }
 
     public static void mostrarMenuSocios() {
@@ -27,7 +32,7 @@ public class Menu {
         System.out.println("1. Insertar socio");
         System.out.println("2. Eliminar socio");
         System.out.println("3. Ver socios");
-        System.out.println("4. Volver al menú principal");
+        System.out.println(SALIR +". Volver al menú principal");
     }
 
     public static void mostrarMenuPrestamos() {
@@ -35,6 +40,6 @@ public class Menu {
         System.out.println("1. Realizar préstamo");
         System.out.println("2. Devolver préstamo");
         System.out.println("3. Ver préstamos");
-        System.out.println("4. Volver al menú principal");
+        System.out.println(SALIR +". Volver al menú principal");
     }
 }
