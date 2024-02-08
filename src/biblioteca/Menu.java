719fca775e8ec9a661ed47806a213c76086fcfd8
benat merino <ikctr@plaiaundi.net>
2024-02-08 10:22:12 +0100

    clase menu terminada

--- src/biblioteca/Menu.java
+++ src/biblioteca/Menu.java
@@ -12,6 +12,14 @@ public class Menu {
 	public static final int ELIMINAR_LIBRO=2;
 	public static final int VER_LIBRO=3;
 	
+	public static final int INSERTAR_SOCIO=1;
+	public static final int ELIMINAR_SOCIO=2;
+	public static final int VER_SOCIO=3;
+	
+	public static final int INSERTAR_PRESTAMO=1;
+	public static final int ELIMINAR_PRESTAMO=2;
+	public static final int VER_PRESTAMO=3;
+	
     public static void mostrarMenuPrincipal() {
         System.out.println("Menu Principal");
         System.out.println(GESTION_LIBROS + ". Gestionar Libros");
@@ -29,17 +37,17 @@ public class Menu {
 
     public static void mostrarMenuSocios() {
         System.out.println("Menu Socios");
-        System.out.println("1. Insertar socio");
-        System.out.println("2. Eliminar socio");
-        System.out.println("3. Ver socios");
+        System.out.println(INSERTAR_SOCIO +". Insertar socio");
+        System.out.println(ELIMINAR_SOCIO +". Eliminar socio");
+        System.out.println(VER_SOCIO +". Ver socios");
         System.out.println(SALIR +". Volver al menú principal");
     }
 
     public static void mostrarMenuPrestamos() {
         System.out.println("Menu Prestamos");
-        System.out.println("1. Realizar préstamo");
-        System.out.println("2. Devolver préstamo");
-        System.out.println("3. Ver préstamos");
+        System.out.println(INSERTAR_PRESTAMO +". Realizar préstamo");
+        System.out.println(ELIMINAR_PRESTAMO +". Devolver préstamo");
+        System.out.println(VER_PRESTAMO +". Ver préstamos");
         System.out.println(SALIR +". Volver al menú principal");
     }
 }
